ea9e17bd1f439ff5139885dae9f34251ec96310b
Thirumaran Asokan <5asokt12@solent.ac.uk>
2022-07-24 04:46:56 +0530

    Completed .

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const buyPriceLabel = document.querySelector(".price-on-date");
 const submit = document.querySelector(".check");
 const process = document.querySelector(".timeout")
 
+
 const currentPriceURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
 const historicalPriceURL = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=";
-
-
 const clickHandler = () => {
     if (purchaseDate.value && quantity.value) {
         showProcess();
@@ -18,19 +17,15 @@ const clickHandler = () => {
         updateResult("Invalid Date or Quantity", "grey");
     }
 }
-
 async function fetchPrices() {
     let boughtAt = 0;
     let currentPrice = 0;
-
     await fetch(currentPriceURL)
         .then(response => response.json())
         .then(data => currentPrice = data["bitcoin"]["usd"]);
-
     await fetch(historicalPriceURL + formatDate(purchaseDate.value))
         .then(response => response.json())
         .then(data => boughtAt = Math.round(data["market_data"]["current_price"]["usd"]));
-
     setTimeout(() => {
         hideProcess();
     }, 1000);
