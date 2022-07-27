82143a595b70f4433189bfc0bb9038a80d57113d
Thirumaran Asokan <5asokt12@solent.ac.uk>
2022-07-27 23:21:23 +0530

    waiting for app

---
+++ Multiple currency/script.js
@@ -0,0 +1,126 @@
+const histories = localStorage.getItem('histories') ? JSON.parse(localStorage.getItem('histories')) : [];
+localStorage.setItem('histories', JSON.stringify(histories))
+
+const getCryptos = async () => {
+	const API_URL = `https://api.coingecko.com/api/v3/coins`;
+	console.log(API_URL)
+	const response = await fetch(API_URL);
+	if (!response.ok) {
+		throw new Error (`An error occurred: ${response.status}`);
+	}
+	return response.json();
+};
+
+const makeSelectOptions = (cryptocurrencies) => {
+	const options = cryptocurrencies.map(( cryptocurrency, index) => {
+		return `<option value="${index}">${cryptocurrency.name}</option>`;
+	});
+
+	const selectCrypto = document.querySelector('#select-crypto');
+	const optionsWithTitle = `<option selected>Choose Crypto currency</option>${options.join('')}`;
+	selectCrypto.innerHTML = optionsWithTitle;
+}
+
+const calculateReturns = (currentPrice, cost, quantity) => {
+	const returns = ((currentPrice * quantity) - (cost * quantity)).toFixed(2);
+	const totalCost = cost * quantity;
+	const total = (Number(totalCost) + Number(returns)).toFixed(2);
+	return {
+		returns,
+		totalCost,
+		total
+	}
+}
+
+const check = (cryptocurrencies, history = {}) => {
+
+	const check = document.querySelector('.check');
+	check.addEventListener('click', (event) => {
+		event.preventDefault();
+		display(cryptocurrencies, history)
+	})
+}
+
+const display = (cryptocurrencies, history) => {
+	const data = resolveData(cryptocurrencies, history);
+	const view = makeView(data);
+
+	const row = document.createElement('li')
+	const list = document.querySelector('.list')
+	row.innerHTML = view;
+	insertAfter(row, list.childNodes[0].nextSibling)
+}
+
+const resolveData = (cryptocurrencies, history = {}) => {
+	const selectedCryptoEl = document.querySelector('#select-crypto');
+	const index = history.index ?? selectedCryptoEl.value;
+	const cryptocurrency = cryptocurrencies[index]
+
+	const price = cryptocurrency.market_data.current_price.usd;
+
+	const costInput = document.querySelector('.price');
+	const cost = history.cost ?? costInput.value;
+
+	const quantityInput = document.querySelector('.quantity');
+	const quantity = history.quantity ?? quantityInput.value;
+	const estimate = calculateReturns(price, cost, quantity);
+
+	if(Object.entries(history).length === 0){
+		store({index, quantity, cost})
+	}
+
+	costInput.value = "";
+	quantityInput.value = "";
+
+	return {
+		cryptocurrency,
+		estimate,
+		index
+	}
+}
+
+const makeView = (data) => `
+		<li class="list__item">
+			<div class="list__grid">
+				<div class="media">
+					<img class="avatar media__img" src="${data.cryptocurrency.image.thumb}" />
+					<div class="media__content">
+						<div class="media__title">${data.cryptocurrency.symbol}</div>
+						<h5>${data.cryptocurrency.name}</h5>
+					</div>
+				</div>
+				<div class="text--right">
+					<div class="mt--8">
+						<span class="text--dark">invested :</span>  <strong class="text--primary">$${data.estimate.totalCost}</strong>
+					</div>
+					<div class="mt--8 ${isGain(data.estimate.returns) ? 'text--success' : 'text--orange'}">
+					${isGain(data.estimate.returns) ? 'gain' : 'loss'} :  <strong>$${Math.abs(data.estimate.returns)}</strong>
+					</div>
+					<div class="mt--8 text--dark">
+					  Total: <strong>$${Math.abs(data.estimate.total)}</strong>
+					</div>
+				</div>
+			</div>
+		</li>
+		`;
+
+const insertAfter = (newNode, existingNode) => existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+
+const store = (history) => {
+	histories.push(history);
+	localStorage.setItem('histories', JSON.stringify(histories));
+}
+
+const isGain = (returns) => {
+	return Math.sign(returns) == 1 ? true : false;
+}
+
+const init = async () => {
+	const cryptocurrencies = await getCryptos();
+	makeSelectOptions(cryptocurrencies);
+
+	histories.forEach( history => display(cryptocurrencies, history));
+	check(cryptocurrencies);
+}
+
+init();
